2b39482fc7f8d400de93908a644a39b076aba304
JasonHonor <jasonhonor@outlook.com>
2023-05-20 22:30:46 +0800

    add unit test of gmssl_version_str.

--- testcrate/src/lib.rs
+++ testcrate/src/lib.rs
@@ -1,15 +1,27 @@
 extern crate libc;
 
-use libc::c_ulong;
+use std::ffi::{c_schar, CString};
+use libc::{c_char, c_ulong};
 
 extern "C" {
     pub fn gmssl_version_num() -> c_ulong;
+    pub fn gmssl_version_str() -> *mut c_char;
 }
 
 #[test]
-fn version_works() {
+fn version_code_works() {
     unsafe {
-        println!("{:#x}", gmssl_version_num());
+        println!("{}", gmssl_version_num());
         assert!(gmssl_version_num() > 0);
+    }
+}
+
+#[test]
+fn version_name_works() {
+    unsafe {
+        let ret = gmssl_version_str();
+        let ret_str = std::ffi::CStr::from_ptr(ret);
+        println!("{:?}", ret_str);
+        assert!(ret_str==std::ffi::CString::new("GmSSL 3.1.1 Dev").unwrap_or(std::ffi::CString::default()).as_c_str());
     }
 }
